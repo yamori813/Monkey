68ee2d7deb3022ee5b22b108af0f7bc69c16fdd2
himori <himori@example.com>
2012-01-19 22:53:10 +0000

    add scale and save to jpeg image
    git-svn-id: svn://svn.code.sf.net/p/ubsa-osx/svn/Monkey@245 c0ceaddf-8823-4bce-a8de-5f6b89fa8458

--- WaveView.m
+++ WaveView.m
@@ -11,6 +11,8 @@
 #import "MonkeyAppDelegate.h"
 #import "MyDocument.h"
 
+#define OFFSETX 10
+#define OFFSETY 10
 
 @implementation WaveView
 
@@ -21,6 +23,27 @@ CGRect convertToCGRect(NSRect inRect)
     return CGRectMake(inRect.origin.x, inRect.origin.y, inRect.size.width, inRect.size.height);
 }
 
+- (void)drawScale:(NSSize) size
+{
+	char strbuf[32];
+	int j;
+	int x = size.width - OFFSETX * 2;
+	int y = size.height - OFFSETY * 2;
+	CGContextSetRGBStrokeColor( gc, 255, 255, 255, 0.3);
+	for(j = 0;j <= 8; ++j) {
+		CGContextMoveToPoint(gc, OFFSETX, OFFSETY + j * y / 8);
+		CGContextAddLineToPoint(gc, OFFSETX + x, OFFSETY + j * y / 8);
+	}
+	
+	CGContextMoveToPoint(gc, OFFSETX, OFFSETY);
+	CGContextAddLineToPoint(gc, OFFSETX, OFFSETY + y);
+	for(j = 1; j <= 12; j += 1) {
+		CGContextMoveToPoint(gc, OFFSETX + j*x/12, OFFSETY);
+		CGContextAddLineToPoint(gc, OFFSETX + j*x/12, OFFSETY + y);
+	}
+	CGContextStrokePath(gc);
+}
+
 - (void)drawWave:(NSData *)thedata
 {
 	int i;
@@ -28,9 +51,9 @@ CGRect convertToCGRect(NSRect inRect)
 	CGContextSetRGBStrokeColor(
 							   gc,255/255.0f,255/255.0f,0/255.0f,1.0f);
 	[thedata getBytes:buff length:sizeof(buff)];
-	CGContextMoveToPoint(gc, 4, 400 - (buff[4] - 28)*2);
+	CGContextMoveToPoint(gc, OFFSETX, 400 - (buff[4] - 28)*2+OFFSETY);
 	for(i = 5; i < 604; ++i) {
-		CGContextAddLineToPoint(gc, i, 400 - (buff[i] - 28)*2); 
+		CGContextAddLineToPoint(gc, i-4+OFFSETX, 400 - (buff[i] - 28)*2+OFFSETY); 
 	}
 	CGContextStrokePath(gc);
 }
@@ -42,6 +65,7 @@ CGRect convertToCGRect(NSRect inRect)
 	CGContextSetGrayFillColor(gc, 0.0, 1.0);
 //	CGContextSetGrayFillColor(gc, 1.0, 1.0);
 	CGContextFillRect(gc, convertToCGRect(rect));
+	[self drawScale:rect.size];
 	MyDocument *thedoc = [[[self window] windowController] document];
 	[self drawWave:[thedoc getData]];
 }
